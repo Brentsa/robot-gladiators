def3c91cee39bdc31928d337b6805ad75ef6f9d5
Solidshots <seanbrent5@gmail.com>
2021-03-01 07:26:22 -0500

    First round of fight is functional

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,36 @@
-window.alert("This is an alert! Javascript is running!");
+
+var playerName = window.prompt("What is your robot's name?");
+var health = 100;
+var attack = 10;
+
+var enemyBot = "Roborto"
+var enemyHealth = 50;
+var enemyAttack = 12;
+
+
+function fight(){
+    window.alert("The fight has begun!");
+    window.alert(playerName + " and " + enemyBot + " have entered the arena");
+    
+    enemyHealth -= attack;
+    window.alert(playerName + " dealt " + attack + " damage!");
+
+    if(enemyHealth <= 0){
+        window.alert(enemyBot + " has lost the fight!");
+    }
+    else{
+        window.alert(enemyBot + " has " + enemyHealth + " health remaining.");
+    }
+    
+    health -= enemyAttack;
+    window.alert(enemyBot + " dealt " + enemyAttack + " damage!");
+
+    if(health <= 0){
+        window.alert(playerName + " has lost the fight. Good luck next time!")
+    }
+    else{
+        window.alert(playerName + " has " + health + " health remaining.");
+    }
+}
+
+fight();
