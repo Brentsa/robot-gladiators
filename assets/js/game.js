d8e5d25d9f6e6235bce795d570a7fba13084c363
Solidshots <seanbrent5@gmail.com>
2021-03-03 07:54:57 -0500

    start and end game functions added

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,4 +1,3 @@
-
 //Game States
 //"WIN" - player defeats all enemies
 //  * player fights all enemies
@@ -12,10 +11,9 @@ var playerMoney = 10;
 
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 30;
-var enemyAttack = 10;
+var enemyAttack = 50;
 
-function fight(enemyName)
-{
+function fight(enemyName){
     while(enemyHealth > 0 && health >0){
         var promptFight = prompt("Would you like to fight? Type fight or skip to choose.");
         if(promptFight === "skip" || promptFight === "SKIP"){
@@ -23,11 +21,7 @@ function fight(enemyName)
             if(confirmSkip){
                 playerMoney -= 10;
                 window.alert("Fight against " + enemyName + " skipped, you lose 10 coin!");
-                console.log("Player money = " + playerMoney);
                 break; 
-            }
-            else{
-                fight(enemyName);
             }
         }
 
@@ -47,21 +41,53 @@ function fight(enemyName)
     }
 }
 
+function startGame(){
+    health = 100;
+    attack = 50;
+    playerMoney = 10;
 
-for(var i = 0; i < enemyNames.length; i++)
-{
-    if(health > 0){
-        alert("Welcome to Robot Gladiators! Round: " + (i+1));   
-        
-        var selectedEnemy = enemyNames[i];
-        enemyHealth = 30;
+    for(var i = 0; i < enemyNames.length; i++)
+    {
+        if(health > 0){
+            alert("Welcome to Robot Gladiators! Round: " + (i + 1) + " - " + enemyNames[i]);   
+            
+            var selectedEnemy = enemyNames[i];
+            enemyHealth = 30;
+    
+            fight(selectedEnemy);
+        } 
+        else{
+            window.alert(playerName + " has lost the fight. Good luck next time!");
+            break;
+        }
+    }
 
-        fight(enemyNames[i]);
-    } 
+    endGame();
+}
+
+function endGame(){
+    if(health > 0 && playerMoney > 0){
+        alert("You won the tournament! You finished with a score of: " + playerMoney);
+    }
     else{
-        window.alert(playerName + " has lost the fight. Good luck next time!");
-        break;
+        alert("You've lost the tournament, better luck next time!");
+    }
+    
+    var exitGame = confirm("Would you like to play Robot Gladiators again?");
+
+    if(exitGame){
+        startGame();
+    }
+    else{
+        alert("Thanks for playing, come back soon!");
     }
 }
+
+startGame();
+
+
+
+
+
 
 
